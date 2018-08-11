9fed2f4d838cbc949dce9cb7d5437147ce8eaa42
Ricky Liu <rorix12321@gmail.com>
2018-08-11 04:21:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+def countdown(number)
+  counter = number
+  while counter >= 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  if counter == 0
+    return "HAPPY NEW YEAR!"
+end
+end
+end
 
-def countdown
+def countdown_with_sleep(number)
+  counter = number
+  while counter >= 0
+    puts "#{counter} SECOND(S)!"
+    sleep(1.second)
+    counter -= 1
+  end
 end
