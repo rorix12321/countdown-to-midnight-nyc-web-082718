f290c142b9b514729bb4b3486cf0325005190f25
Ricky Liu <rorix12321@gmail.com>
2018-08-11 04:22:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,9 +11,12 @@ end
 
 def countdown_with_sleep(number)
   counter = number
+  if counter < 5
+    counter += 1
   while counter >= 0
     puts "#{counter} SECOND(S)!"
     sleep(1.second)
     counter -= 1
   end
+end
 end
